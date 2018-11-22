8b6127530f963d4909d502df1447edea9e28c659
liuyi <liuyi5683@163.com>
2018-11-22 16:17:05 +0800

    更新

--- HDMiusiSpace/Extension/CoreLocation+Extension.swift
+++ HDMiusiSpace/Extension/CoreLocation+Extension.swift
@@ -23,7 +23,7 @@ let ee:Double = 0.00669342162296594323
 // --- transform_earth_from_mars end ---
 // --- transform_mars_vs_bear_paw ---
 // 参考来源：http://blog.woodbunny.com/post-68.html
-let x_pi:Double = M_PI * 3000.0 / 180.0
+let x_pi:Double = Double.pi * 3000.0 / 180.0
 
 
 /*
@@ -106,26 +106,26 @@ public extension CLLocation {
         }
         var dLat = CLLocation.transform_earth_from_mars_lat(lng - 105.0, lat - 35.0)
         var dLng = CLLocation.transform_earth_from_mars_lng(lng - 105.0, lat - 35.0)
-        let radLat = lat / 180.0 * M_PI
+        let radLat = lat / 180.0 * Double.pi
         var magic = sin(radLat)
         magic = 1 - ee * magic * magic
         let sqrtMagic:Double = sqrt(magic)
-        dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * M_PI)
-        dLng = (dLng * 180.0) / (a / sqrtMagic * cos(radLat) * M_PI)
+        dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * Double.pi)
+        dLng = (dLng * 180.0) / (a / sqrtMagic * cos(radLat) * Double.pi)
         return (dLat,dLng)
     }
     private class func transform_earth_from_mars_lat(_ x: Double,_ y: Double) -> Double {
         var ret: Double = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * sqrt(fabs(x))
-        ret += (20.0 * sin(6.0 * x * M_PI) + 20.0 * sin(2.0 * x * M_PI)) * 2.0 / 3.0
-        ret += (20.0 * sin(y * M_PI) + 40.0 * sin(y / 3.0 * M_PI)) * 2.0 / 3.0
-        ret += (160.0 * sin(y / 12.0 * M_PI) + 320 * sin(y * M_PI / 30.0)) * 2.0 / 3.0
+        ret += (20.0 * sin(6.0 * x * Double.pi) + 20.0 * sin(2.0 * x * Double.pi)) * 2.0 / 3.0
+        ret += (20.0 * sin(y * Double.pi) + 40.0 * sin(y / 3.0 * Double.pi)) * 2.0 / 3.0
+        ret += (160.0 * sin(y / 12.0 * Double.pi) + 320 * sin(y * Double.pi / 30.0)) * 2.0 / 3.0
         return ret
     }
     private class func transform_earth_from_mars_lng(_ x: Double,_ y: Double) -> Double {
         var ret: Double = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * sqrt(fabs(x))
-        ret += (20.0 * sin(6.0 * x * M_PI) + 20.0 * sin(2.0 * x * M_PI)) * 2.0 / 3.0
-        ret += (20.0 * sin(x * M_PI) + 40.0 * sin(x / 3.0 * M_PI)) * 2.0 / 3.0
-        ret += (150.0 * sin(x / 12.0 * M_PI) + 300.0 * sin(x / 30.0 * M_PI)) * 2.0 / 3.0
+        ret += (20.0 * sin(6.0 * x * Double.pi) + 20.0 * sin(2.0 * x * Double.pi)) * 2.0 / 3.0
+        ret += (20.0 * sin(x * Double.pi) + 40.0 * sin(x / 3.0 * Double.pi)) * 2.0 / 3.0
+        ret += (150.0 * sin(x / 12.0 * Double.pi) + 300.0 * sin(x / 30.0 * Double.pi)) * 2.0 / 3.0
         return ret
     }
     private class func transform_mars_from_baidu(lat: Double, lng: Double) -> (Double,Double) {

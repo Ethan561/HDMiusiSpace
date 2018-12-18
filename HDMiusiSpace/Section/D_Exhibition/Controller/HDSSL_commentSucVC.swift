c4404b2b3a85922c577d7a4440c123cd265f5aec
sunshilong <1577908011@qq.com>
2018-12-18 18:45:55 +0800

    画报分享，ssl

--- HDMiusiSpace/Section/D_Exhibition/Controller/HDSSL_commentSucVC.swift
+++ HDMiusiSpace/Section/D_Exhibition/Controller/HDSSL_commentSucVC.swift
@@ -109,7 +109,13 @@ class HDSSL_commentSucVC: HDItemBaseVC {
     }
     @IBAction func action_shareMyComment(_ sender: Any) {
         //分享
-        
+        //分享
+        let tipView: HDLY_ShareView = HDLY_ShareView.createViewFromNib() as! HDLY_ShareView
+        tipView.frame = CGRect.init(x: 0, y: 0, width: ScreenWidth, height: ScreenHeight)
+        tipView.delegate = self
+        if kWindow != nil {
+            kWindow!.addSubview(tipView)
+        }
     }
     
     func request_createPaper() {
@@ -192,4 +198,42 @@ extension HDSSL_commentSucVC:UITableViewDelegate,UITableViewDataSource {
     }
     
     
+}
+//MARK:--- 分享
+extension HDSSL_commentSucVC: UMShareDelegate {
+    func shareDelegate(platformType: UMSocialPlatformType) {
+        
+        guard let url  = self.htmlShareUrl else {
+            return
+        }
+        
+        //创建分享消息对象
+        let messageObject = UMSocialMessageObject()
+        //创建网页内容对象
+        let thumbURL = url
+        let shareObject = UMShareWebpageObject.shareObject(withTitle: "缪斯空间", descr: "归属感，缪斯空间", thumImage: thumbURL)
+        
+        //设置网页地址
+        shareObject?.webpageUrl = url
+        //分享消息对象设置分享内容对象
+        messageObject.shareObject = shareObject
+        
+        UMSocialManager.default().share(to: platformType, messageObject: messageObject, currentViewController: self) { data, error in
+            if error != nil {
+                //UMSocialLog(error)
+                LOG(error)
+            } else {
+                if (data is UMSocialShareResponse) {
+                    let resp = data as? UMSocialShareResponse
+                    //分享结果消息
+                    LOG(resp?.message)
+                    
+                    //第三方原始返回的数据
+                    print(resp?.originalResponse)
+                } else {
+                    LOG(data)
+                }
+            }
+        }
+    }
 }

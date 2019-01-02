9769090e9a1ecd75135e0e273b20edc93e31a17e
sunshilong <1577908011@qq.com>
2019-01-02 15:36:42 +0800

    ssl

---
+++ HDMiusiSpace/Section/D_Exhibition/Controller/HDSSL_StrategyDetialVC.swift
@@ -0,0 +1,37 @@
+//
+//  HDSSL_StrategyDetialVC.swift
+//  HDMiusiSpace
+//
+//  Created by SSLong on 2019/1/2.
+//  Copyright © 2019 hengdawb. All rights reserved.
+//
+
+import UIKit
+
+class HDSSL_StrategyDetialVC: HDItemBaseVC {
+
+    @IBOutlet weak var myTableView: UITableView!
+    @IBOutlet weak var commentBgView: UIView!
+    @IBOutlet weak var textBgView: UIView!
+    @IBOutlet weak var likeBtn: UIButton!
+    @IBOutlet weak var collectionBtn: UIButton!
+    @IBOutlet weak var likeNumL: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

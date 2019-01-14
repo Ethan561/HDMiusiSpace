9376adcbbdc1a9c8e098a1c58b19749b7d53a6e8
sunshilong <1577908011@qq.com>
2019-01-14 16:28:11 +0800

    添加暂无评论

---
+++ HDMiusiSpace/Section/D_Exhibition/View/Cell/Comment/HDSSL_noCommentCell.swift
@@ -0,0 +1,33 @@
+//
+//  HDSSL_noCommentCell.swift
+//  HDMiusiSpace
+//
+//  Created by SSLong on 2019/1/14.
+//  Copyright © 2019 hengdawb. All rights reserved.
+//  没有评论时
+
+import UIKit
+
+class HDSSL_noCommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    class  func getMyTableCell(tableV: UITableView) -> HDSSL_noCommentCell! {
+        var cell: HDSSL_noCommentCell? = tableV.dequeueReusableCell(withIdentifier: HDSSL_noCommentCell.className) as? HDSSL_noCommentCell
+        if cell == nil {
+            //注册cell
+            tableV.register(UINib.init(nibName: HDSSL_noCommentCell.className, bundle: nil), forCellReuseIdentifier: HDSSL_noCommentCell.className)
+            cell = Bundle.main.loadNibNamed(HDSSL_noCommentCell.className, owner: nil, options: nil)?.first as? HDSSL_noCommentCell
+        }
+        cell?.selectionStyle = UITableViewCellSelectionStyle.none
+        return cell!
+    }
+}

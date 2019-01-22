3330843a3f3afe61fd4c9850ac513b08470214db
XXXXX <939568547@qq.com>
2019-01-22 14:08:59 +0800

    资讯次级评论删除bug修复

--- HDMiusiSpace/Section/B_NewKnowledge/Controller/HDLY_TopicDetail_VC.swift
+++ HDMiusiSpace/Section/B_NewKnowledge/Controller/HDLY_TopicDetail_VC.swift
@@ -550,7 +550,8 @@ extension HDLY_TopicDetail_VC {
                     let returnArr = commentModel.list
                     if returnArr.count > 0{
                         for returnModel in returnArr {
-                            if returnModel.uid == HDDeclare.shared.uid {
+                            
+                            if returnModel.uid == HDDeclare.shared.uid && returnModel.commentID == commentId {
                                 //自己的回复，模型转换
                                 let model1 = TopicCommentList.init(uid: returnModel.uid, comment: returnModel.comment, likeNum: commentModel.likeNum, createdAt: "", commentID: returnModel.commentID, avatar: "", nickname: returnModel.uNickname, isLike: 0, list: [], showAll: true, height: 44, topHeight: 64)
                                 self?.commentView.model = model1
